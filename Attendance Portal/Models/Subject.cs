4d407ba67a1e465d5b98619e5a85661f2091a946
Aditya Singh <adityasingh2002_@outlook.com>
2022-12-15 22:34:40 -0800

    Working on Modal

---
+++ Attendance Portal/Models/Subject.cs
@@ -0,0 +1,9 @@
+﻿namespace Attendance_Portal.Models
+{
+    public class Subject
+    {
+        public short SubjectCode { get; set; }
+        public string SubjectName { get; set; }
+        public short SemesterCode { get; set; }
+    }
+}
